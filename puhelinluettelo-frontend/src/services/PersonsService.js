b78e4c91cdb34bb86609c629b81b4263cd27b0f2
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-03-29 10:32:59 +0300

    working backend and frontend

--- puhelinluettelo-frontend/src/services/PersonsService.js
+++ puhelinluettelo-frontend/src/services/PersonsService.js
@@ -1,29 +1,29 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
-    const request = axios.get(baseUrl)
-    return request.then(response => response.data)
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
 
 const create = newObject => {
-    const request = axios.post(baseUrl, newObject)
-    return request.then(response => response.data)
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
 }
 
 const delPerson = id => {
-    const delUrl = `${baseUrl}/${id}`
-    const request = axios.delete(delUrl)
+  const delUrl = `${baseUrl}/${id}`
+  const request = axios.delete(delUrl)
 
-    return request.then(response => response.status)
+  return request.then(response => response.status)
 }
 
 const changePersonNumber = (existingPersonObj, newNumber) => {
-    const changeUrl = `${baseUrl}/${existingPersonObj.id}`
-    const changedPersonObj = { ...existingPersonObj, number: newNumber }
-    const request = axios.put(changeUrl, changedPersonObj)
+  const changeUrl = `${baseUrl}/${existingPersonObj.id}`
+  const changedPersonObj = { ...existingPersonObj, number: newNumber }
+  const request = axios.put(changeUrl, changedPersonObj)
 
-    return request.then(response => response.data)
+  return request.then(response => response.data)
 }
 
 export default { getAll, create, delPerson, changePersonNumber }
